04d8a3fd9244bbd4bd7063eae953d24995a81a7f
Otto Link <otto.link.bv@gmail.com>
2023-05-14 09:37:06 +0200

    Change cmake files to generate a library

---
+++ scripts/generate_cmakelists_examples.py
@@ -0,0 +1,30 @@
+import glob
+import os
+
+PATHS = ['examples/']
+LIB_NAME = 'highmap'
+
+STUB = '''add_executable({} {}.cpp)
+target_link_libraries({} {})
+'''
+
+if __name__ == '__main__':
+
+    for p in PATHS:
+        exdirs = glob.glob(p + '/ex_*')
+
+        for d in exdirs:
+            if os.path.isdir(d):
+                print(d)
+
+                # generate CMakeLists.txt file
+                example_name = os.path.basename(d)
+                fname_cmake =  os.path.join(d, 'CMakeLists.txt')
+                
+                with open(fname_cmake, 'w') as f:
+                    f.write(
+                        STUB.format(
+                            example_name,
+                            example_name,
+                            example_name,
+                            LIB_NAME))
