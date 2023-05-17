aa480041bfed651dfd8a0ba724e3a1dd3bb55ab8
Otto Link <otto.link.bv@gmail.com>
2023-05-17 20:51:27 +0200

    Format code

--- examples/ex_hydraulic_particle_multiscale/ex_hydraulic_particle_multiscale.cpp
+++ examples/ex_hydraulic_particle_multiscale/ex_hydraulic_particle_multiscale.cpp
@@ -8,16 +8,20 @@ int main(void)
 {
   const std::vector<int>   shape = {256, 256};
   const std::vector<float> res = {2.f, 2.f};
-  float particle_density = 0.3f;
+  float                    particle_density = 0.3f;
   int                      seed = 1;
 
   hmap::Array z = hmap::fbm_perlin(shape, res, seed);
   auto        z0 = z;
-  auto moisture_map = z;
+  auto        moisture_map = z;
 
   hmap::remap(moisture_map);
 
-  hmap::hydraulic_particle_multiscale(z, moisture_map, particle_density, {8.f, 4.f, 2.f}, seed);
+  hmap::hydraulic_particle_multiscale(z,
+                                      moisture_map,
+                                      particle_density,
+                                      {8.f, 4.f, 2.f},
+                                      seed);
 
   hmap::export_banner_png("ex_hydraulic_particle_multiscale.png",
                           {z0, z},
