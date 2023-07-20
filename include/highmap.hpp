3d256a9c1179158ed6ce10a0a843c484e3a0d0a4
Otto Link <otto.link.bv@gmail.com>
2023-07-20 18:49:14 +0200

    Add a unique header highmap.hpp on top of all the specific includes, for conveniency. (#8)

---
+++ include/highmap.hpp
@@ -0,0 +1,21 @@
+/**
+ * @file highmap.hpp
+ * @author Otto Link (otto.link.bv@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2023-07-20
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+#pragma once
+
+#include "highmap/array.hpp"
+#include "highmap/colormaps.hpp"
+#include "highmap/erosion.hpp"
+#include "highmap/geometry.hpp"
+#include "highmap/hydrology.hpp"
+#include "highmap/io.hpp"
+#include "highmap/op.hpp"
+#include "highmap/primitives.hpp"
+#include "highmap/roads.hpp"
