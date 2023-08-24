a1bc90cefc13a071a69bad3836c7481658336408
Otto Link <otto.link.bv@gmail.com>
2023-08-24 20:19:45 +0200

    Add timer to sandbox test sources

--- tests/sandbox/main.cpp
+++ tests/sandbox/main.cpp
@@ -1,15 +1,17 @@
-#include "highmap/array.hpp"
-#include "highmap/io.hpp"
-#include "highmap/primitives.hpp"
+#include "highmap.hpp"
+#include "highmap/dbg.hpp"
 
 int main(void)
 {
+  hmap::Timer timer = hmap::Timer();
 
   const hmap::Vec2<int>   shape = {512, 512};
   const hmap::Vec2<float> res = {4.f, 4.f};
   int                     seed = 2;
 
+  timer.start("fbm_perlin");
   hmap::Array z = hmap::fbm_perlin(shape, res, seed, 8);
+  timer.stop("fbm_perlin");
 
   z.to_png("out.png", hmap::cmap::gray);
 }
