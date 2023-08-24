4154d2c117102bc693bd57ed5a1a5f0692adccdf
Otto Link <otto.link.bv@gmail.com>
2023-08-24 19:55:22 +0200

    Add checkerboard primitive (#39)

---
+++ examples/ex_checkerboard/ex_checkerboard.cpp
@@ -0,0 +1,11 @@
+#include "highmap.hpp"
+
+int main(void)
+{
+  hmap::Vec2<int>   shape = {256, 256};
+  hmap::Vec2<float> kw = {4.f, 3.f};
+
+  hmap::Array z = hmap::checkerboard(shape, kw);
+
+  z.to_png("ex_checkerboard.png", hmap::cmap::gray);
+}
