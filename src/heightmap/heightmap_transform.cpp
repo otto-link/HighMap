2b47cda8a5a56a47812bba303917c446beddf6fb
Otto Link <otto.link.bv@gmail.com>
2023-08-25 14:44:29 +0200

    Add quaternary operator transform for heightmap class

--- src/heightmap/heightmap_transform.cpp
+++ src/heightmap/heightmap_transform.cpp
@@ -235,4 +235,26 @@ void transform(HeightMap                                     &h1,
     futures[i].get();
 }
 
+void transform(
+    HeightMap                                              &h1,
+    HeightMap                                              &h2,
+    HeightMap                                              &h3,
+    HeightMap                                              &h4,
+    std::function<void(Array &, Array &, Array &, Array &)> quaternary_op)
+{
+  LOG_DEBUG("quaternary");
+  size_t                         nthreads = h1.get_ntiles();
+  std::vector<std::future<void>> futures(nthreads);
+
+  for (decltype(futures)::size_type i = 0; i < nthreads; ++i)
+    futures[i] = std::async(quaternary_op,
+                            std::ref(h1.tiles[i]),
+                            std::ref(h2.tiles[i]),
+                            std::ref(h3.tiles[i]),
+                            std::ref(h4.tiles[i]));
+
+  for (decltype(futures)::size_type i = 0; i < nthreads; ++i)
+    futures[i].get();
+}
+
 } // namespace hmap
