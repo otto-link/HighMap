422aa7ebee02c16ceb88ff0c8dddd80b7959e4c2
Otto Link <otto.link.bv@gmail.com>
2023-09-02 12:55:33 +0200

    Add example

--- examples/ex_alter_elevation/ex_alter_elevation.cpp
+++ examples/ex_alter_elevation/ex_alter_elevation.cpp
@@ -11,13 +11,16 @@ int main(void)
   // modification point (x, y) in [0, 1]x[0, 1] and a value
   // corresponding to the relative elevation modification
   hmap::Cloud cloud = hmap::Cloud();
-  cloud.add_point(hmap::Point(0.5f, 0.5f, -0.5f));
-  cloud.add_point(hmap::Point(0.6f, 0.2f, 0.2f));
+  cloud.add_point(hmap::Point(0.2f, 0.5f, -1.f));
+  cloud.add_point(hmap::Point(0.6f, 0.2f, 1.f));
 
   hmap::Array z1 = z0;
   hmap::alter_elevation(z1, cloud, 32, 2.f);
 
   z1.to_png("out.png", hmap::cmap::inferno);
+
+  z0.infos();
+  z1.infos();
 
   hmap::export_banner_png("ex_alter_elevation.png",
                           {z0, z1},
