05d5a02af9d6cc25cb8f397cb4ec9dd98218684e
Otto Link <otto.link.bv@gmail.com>
2023-09-07 18:33:07 +0200

    Check setter new values before applying changes

--- src/heightmap/heightmap_base.cpp
+++ src/heightmap/heightmap_base.cpp
@@ -82,10 +82,14 @@ void HeightMap::set_sto(Vec2<int> new_shape,
                         Vec2<int> new_tiling,
                         float     new_overlap)
 {
-  this->shape = new_shape;
-  this->tiling = new_tiling;
-  this->overlap = new_overlap;
-  this->update_tile_parameters();
+  if ((this->shape != new_shape) || (this->tiling != new_tiling) ||
+      (this->overlap != new_overlap))
+  {
+    this->shape = new_shape;
+    this->tiling = new_tiling;
+    this->overlap = new_overlap;
+    this->update_tile_parameters();
+  }
 }
 
 void HeightMap::set_tiling(Vec2<int> new_tiling)
