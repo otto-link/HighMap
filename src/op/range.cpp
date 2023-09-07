1d7a4a926cc1d055a4016b7453b47c60300e9096
Otto Link <otto.link.bv@gmail.com>
2023-09-07 23:04:52 +0200

    Add failsafe to remap to avoid nan

--- src/op/range.cpp
+++ src/op/range.cpp
@@ -351,24 +351,34 @@ void remap(Array &array, float vmin, float vmax)
   float min = array.min();
   float max = array.max();
 
-  auto lambda = [&min, &max, &vmin, &vmax](float x)
-  { return (x - min) / (max - min) * (vmax - vmin) + vmin; };
+  if (min != max)
+  {
+    auto lambda = [&min, &max, &vmin, &vmax](float x)
+    { return (x - min) / (max - min) * (vmax - vmin) + vmin; };
 
-  std::transform(array.vector.begin(),
-                 array.vector.end(),
-                 array.vector.begin(),
-                 lambda);
+    std::transform(array.vector.begin(),
+                   array.vector.end(),
+                   array.vector.begin(),
+                   lambda);
+  }
+  else
+    std::fill(array.vector.begin(), array.vector.end(), vmin);
 }
 
 void remap(Array &array, float vmin, float vmax, float from_min, float from_max)
 {
-  auto lambda = [&from_min, &from_max, &vmin, &vmax](float x)
-  { return (x - from_min) / (from_max - from_min) * (vmax - vmin) + vmin; };
+  if (from_min != from_max)
+  {
+    auto lambda = [&from_min, &from_max, &vmin, &vmax](float x)
+    { return (x - from_min) / (from_max - from_min) * (vmax - vmin) + vmin; };
 
-  std::transform(array.vector.begin(),
-                 array.vector.end(),
-                 array.vector.begin(),
-                 lambda);
+    std::transform(array.vector.begin(),
+                   array.vector.end(),
+                   array.vector.begin(),
+                   lambda);
+  }
+  else
+    std::fill(array.vector.begin(), array.vector.end(), vmin);
 }
 
 } // namespace hmap
