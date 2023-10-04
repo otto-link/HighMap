af34954b858288ac9226a13a419d00aeca3ba10e
Otto Link <otto.link.bv@gmail.com>
2023-10-04 16:35:12 +0200

    Switch to libpng to export 16bit png (#71)

--- examples/ex_to_png/ex_to_png.cpp
+++ examples/ex_to_png/ex_to_png.cpp
@@ -16,5 +16,7 @@ int main(void)
   hmap::Array z2 = hmap::Array("out.png");
   z2.infos();
 
+  z2.to_png16bit("out_16bit.png");
+
   hmap::export_banner_png("ex_to_png.png", {z1, z2}, hmap::cmap::inferno);
 }
--- src/array/array_io.cpp
+++ src/array/array_io.cpp
@@ -74,4 +74,98 @@ void Array::to_png(std::string fname, int cmap, bool hillshading)
                  IMG_CHANNELS * this->shape.x);
 }
 
+void Array::to_png16bit(std::string fname)
+{
+  const float vmin = this->min();
+  const float vmax = this->max();
+
+  std::vector<uint16_t> data(this->shape.x * this->shape.y);
+
+  float a = 0.f;
+  float b = 0.f;
+
+  if (vmin != vmax)
+  {
+    a = 1.f / (vmax - vmin);
+    b = -vmin / (vmax - vmin);
+  }
+
+  int k = 0;
+
+  for (int j = this->shape.y - 1; j > -1; j -= 1)
+    for (int i = 0; i < this->shape.x; i += 1)
+    {
+      float v = a * (*this)(i, j) + b;
+      data[k++] = (uint16_t)(v * 65535.f);
+    }
+
+  // ---
+
+  FILE *fp = fopen(fname.c_str(), "wb");
+  if (!fp)
+    LOG_ERROR("Error opening file for writing: %s", fname.c_str());
+
+  png_structp png =
+      png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+  if (!png)
+  {
+    LOG_ERROR("Error creating PNG write structure");
+    fclose(fp);
+  }
+
+  png_infop info = png_create_info_struct(png);
+  if (!info)
+  {
+    LOG_ERROR("Error creating PNG info structure");
+    png_destroy_write_struct(&png, NULL);
+    fclose(fp);
+  }
+
+  if (setjmp(png_jmpbuf(png)))
+  {
+    LOG_ERROR("Error during PNG creation");
+    png_destroy_write_struct(&png, &info);
+    fclose(fp);
+  }
+
+  // Initialize the PNG file
+  png_init_io(png, fp);
+
+  // Set the image properties
+  png_set_IHDR(png,
+               info,
+               this->shape.x,
+               this->shape.y,
+               16,
+               PNG_COLOR_TYPE_GRAY,
+               PNG_INTERLACE_NONE,
+               PNG_COMPRESSION_TYPE_DEFAULT,
+               PNG_FILTER_TYPE_DEFAULT);
+
+  // Write the PNG header
+  png_write_info(png, info);
+
+  // Allocate memory for row pointers
+  int        width = this->shape.x;
+  int        height = this->shape.y;
+  png_bytep *row_pointers = new png_bytep[height];
+  for (int i = 0; i < height; ++i)
+  {
+    row_pointers[i] = (png_bytep)&data[i * width];
+  }
+
+  // Write the image data
+  png_write_image(png, row_pointers);
+
+  // End the PNG write process
+  png_write_end(png, NULL);
+
+  // Clean up
+  png_destroy_write_struct(&png, &info);
+  fclose(fp);
+  delete[] row_pointers;
+
+  LOG_DEBUG("PNG file saved successfully: %s", fname.c_str());
+}
+
 } // namespace hmap
