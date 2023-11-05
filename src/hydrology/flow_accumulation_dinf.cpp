1dd7133647f89c56ddcd8ba98820843334a680be
Otto Link <otto.link.bv@gmail.com>
2023-11-05 15:55:05 +0100

    Modify header to improve portability

--- src/hydrology/flow_accumulation_dinf.cpp
+++ src/hydrology/flow_accumulation_dinf.cpp
@@ -1,9 +1,6 @@
 /* Copyright (c) 2023 Otto Link. Distributed under the terms of the GNU General
  * Public License. The full license is in the file LICENSE, distributed with
  * this software. */
-
-#define _USE_MATH_DEFINES
-
 #include <cmath>
 #include <list>
 
