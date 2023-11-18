ba03f05711013e8d0504817a4a5f8ec1075eb830
Otto Link <otto.link.bv@gmail.com>
2023-11-18 13:27:35 +0100

    Add hydraulic_stream_log to circumvent too flat erosion paths (#82)

--- src/erosion/hydraulic_stream.cpp
+++ src/erosion/hydraulic_stream.cpp
@@ -97,4 +97,83 @@ void hydraulic_stream(Array &z,
   }
 }
 
+void hydraulic_stream_log(Array &z,
+                          float  c_erosion,
+                          float  talus_ref,
+                          float  gamma,
+                          Array *p_bedrock,
+                          Array *p_moisture_map,
+                          Array *p_erosion_map,
+                          int    ir)
+{
+  // keep a backup of the input if the erosion / deposition maps need
+  // to be computed
+  Array z_bckp = Array();
+  if (p_erosion_map)
+    z_bckp = z;
+
+  // use flow accumulation to determine erosion intensity
+  Array facc = flow_accumulation_dinf(z, talus_ref);
+  facc = log10(facc);
+  facc.infos();
+  remap(facc);
+  gamma_correction(facc, gamma);
+
+  if (ir > 1)
+  {
+    Array kernel = cone({ir, ir});
+    kernel.normalize();
+    facc = convolve2d_svd(facc, kernel);
+  }
+
+  if (p_moisture_map)
+    z -= (*p_moisture_map) * c_erosion * facc;
+  else
+    z -= c_erosion * facc;
+
+  if (p_bedrock)
+    z = maximum(*p_bedrock, z);
+
+  // splatmaps
+  if (p_erosion_map)
+  {
+    *p_erosion_map = z_bckp - z;
+    clamp_min(*p_erosion_map, 0.f);
+  }
+}
+
+void hydraulic_stream_log(Array &z,
+                          Array *p_mask,
+                          float  c_erosion,
+                          float  talus_ref,
+                          float  gamma,
+                          Array *p_moisture_map,
+                          Array *p_bedrock,
+                          Array *p_erosion_map,
+                          int    ir)
+{
+  if (!p_mask)
+    hydraulic_stream_log(z,
+                         c_erosion,
+                         talus_ref,
+                         gamma,
+                         p_bedrock,
+                         p_moisture_map,
+                         p_erosion_map,
+                         ir);
+  else
+  {
+    Array z_f = z;
+    hydraulic_stream_log(z_f,
+                         c_erosion,
+                         talus_ref,
+                         gamma,
+                         p_bedrock,
+                         p_moisture_map,
+                         p_erosion_map,
+                         ir);
+    z = lerp(z, z_f, *(p_mask));
+  }
+}
+
 } // namespace hmap
