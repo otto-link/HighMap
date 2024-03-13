d3abee7f60e5530305aaa74c5ab56d5d35ed29ac
Otto Link <otto.link.bv@gmail.com>
2024-03-13 18:03:23 +0100

    Clean-up example

--- examples/ex_sediment_deposition_particle/ex_sediment_deposition_particle.cpp
+++ examples/ex_sediment_deposition_particle/ex_sediment_deposition_particle.cpp
@@ -1,7 +1,4 @@
-#include "highmap/array.hpp"
-#include "highmap/erosion.hpp"
-#include "highmap/io.hpp"
-#include "highmap/primitives.hpp"
+#include "highmap.hpp"
 
 int main(void)
 {
@@ -18,6 +15,4 @@ int main(void)
                           {z0, z},
                           hmap::cmap::terrain,
                           true);
-
-  z.to_file("out.bin");
 }
