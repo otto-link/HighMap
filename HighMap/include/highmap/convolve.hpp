2125f5a0af321a12c483f9aaba11e3b51aa1c3e0
Otto Link <otto.link.bv@gmail.com>
2024-08-21 14:09:18 +0200

    Add doxygen doc

--- HighMap/include/highmap/convolve.hpp
+++ HighMap/include/highmap/convolve.hpp
@@ -5,29 +5,38 @@
 /**
  * @file convolve.hpp
  * @author Otto Link (otto.link.bv@gmail.com)
- * @brief
+ * @brief Provides functions for convolution operations on arrays.
+ *
+ * This header file defines functions for performing convolution operations
+ * on 1D and 2D arrays using various kernels. The operations include 1D
+ * convolutions along different directions, 2D convolutions with symmetry
+ * boundary conditions, truncated 2D convolutions, and approximate convolutions
+ * using Singular Value Decomposition (SVD).
+ *
  * @version 0.1
  * @date 2023-04-30
  *
- * @copyright Copyright (c) 2023
- *
+ * @copyright Copyright (c) 2023 Otto Link
  */
+
 #pragma once
 
-#include "macrologger.h"
-
 #include "highmap/array.hpp"
+#include "macrologger.h"
 
 namespace hmap
 {
 
 /**
- * @brief Return the convolution product of the array with a 1D kernel (row, 'i'
- * direction).
+ * @brief Return the convolution product of the array with a 1D kernel along the
+ * 'i' direction.
  *
- * @param array Input array.
- * @param kernel Kernel (1D).
- * @return Array Convolution result.
+ * This function applies a 1D convolution to the input array using the provided
+ * kernel, considering the convolution in the row direction ('i' direction).
+ *
+ * @param array Input array to be convolved.
+ * @param kernel 1D kernel to be used for the convolution.
+ * @return Array Resulting array after applying the 1D convolution.
  *
  * **Example**
  * @include ex_convolve1d_ij.cpp
@@ -40,12 +49,15 @@ namespace hmap
 Array convolve1d_i(const Array &array, const std::vector<float> &kernel);
 
 /**
- * @brief Return the convolution product of the array with a 1D kernel (column,
- * 'j' direction).
+ * @brief Return the convolution product of the array with a 1D kernel along the
+ * 'j' direction.
  *
- * @param array Input array.
- * @param kernel Kernel (1D).
- * @return Array Convolution result.
+ * This function applies a 1D convolution to the input array using the provided
+ * kernel, considering the convolution in the column direction ('j' direction).
+ *
+ * @param array Input array to be convolved.
+ * @param kernel 1D kernel to be used for the convolution.
+ * @return Array Resulting array after applying the 1D convolution.
  *
  * **Example**
  * @include ex_convolve1d_ij.cpp
@@ -58,12 +70,15 @@ Array convolve1d_i(const Array &array, const std::vector<float> &kernel);
 Array convolve1d_j(const Array &array, const std::vector<float> &kernel);
 
 /**
- * @brief Return the convolution product of the array with a given kernel. The
- * output has the same shape as the input (symmetry boundary conditions).
+ * @brief Return the convolution product of the array with a given 2D kernel.
  *
- * @param array Input array.
- * @param kernel Kernel array.
- * @return Array Convolution result.
+ * This function performs a 2D convolution on the input array using the
+ * specified 2D kernel. The output array has the same shape as the input array,
+ * and symmetry boundary conditions are used.
+ *
+ * @param array Input array to be convolved.
+ * @param kernel 2D kernel to be used for the convolution.
+ * @return Array Resulting array after applying the 2D convolution.
  *
  * **Example**
  * @include ex_convolve2d_svd.cpp
@@ -71,13 +86,18 @@ Array convolve1d_j(const Array &array, const std::vector<float> &kernel);
 Array convolve2d(const Array &array, const Array &kernel);
 
 /**
- * @brief Return the convolution product of the array with a given kernel. The
- * output has a smaller size than the input.
+ * @brief Return the convolution product of the array with a given 2D kernel,
+ * with a truncated output size.
  *
- * @param array Input array.
- * @param kernel Kernel array.
- * @return Array Convolution result (shape: {array.shape[0] - kernel.shape[0],
- * array.shape[1] - kernel.shape[1]}).
+ * This function performs a 2D convolution on the input array using the
+ * specified 2D kernel. The resulting output array is smaller than the input
+ * array by the dimensions of the kernel.
+ *
+ * @param array Input array to be convolved.
+ * @param kernel 2D kernel to be used for the convolution.
+ * @return Array Resulting array after applying the truncated 2D convolution.
+ *         Shape: {array.shape[0] - kernel.shape[0], array.shape[1] -
+ * kernel.shape[1]}.
  */
 Array convolve2d_truncated(const Array &array, const Array &kernel);
 
@@ -85,14 +105,16 @@ Array convolve2d_truncated(const Array &array, const Array &kernel);
  * @brief Return the approximate convolution product of the array with a
  * Singular Value Decomposition (SVD) of a kernel.
  *
- * See reference @cite McGraw2014 and this post
- * https://bartwronski.com/2020/02/03/separate-your-filters-svd-and-low-rank-approximation-of-image-filters/
+ * This function approximates the convolution of the input array with a 2D
+ * kernel by using its Singular Value Decomposition (SVD). The approximation is
+ * based on the first `rank` singular values and vectors.
  *
- * @param z Input array.
- * @param kernel Kernel array.
- * @param rank Approximation rank: the first 'rank' singular values/vectors are
- * used to approximate the convolution product.
- * @return Array Convolution result.
+ * @param z Input array to be convolved.
+ * @param kernel 2D kernel to be used for the convolution.
+ * @param rank Approximation rank: number of singular values/vectors used for
+ * the approximation.
+ * @return Array Resulting array after applying the SVD-based convolution
+ * approximation.
  *
  * **Example**
  * @include ex_convolve2d_svd.cpp
@@ -104,16 +126,22 @@ Array convolve2d_svd(const Array &z, const Array &kernel, int rank = 3);
 
 /**
  * @brief Return the approximate convolution product of the array with a
- * Singular Value Decomposition (SVD) of a kernel combined with a rotation of
- * the kernel.
+ * Singular Value Decomposition (SVD) of a kernel combined with kernel
+ * rotations.
  *
- * @param z Input array.
- * @param kernel Kernel array.
- * @param rank Approximation rank: the first 'rank' singular values/vectors are
- * used to approximate the convolution product.
- * @param n_rotations Number of kernel rotations.
- * @param seed Random seed number.
- * @return Array Convolution result.
+ * This function approximates the convolution of the input array with a 2D
+ * kernel using its SVD, combined with multiple rotations of the kernel. This
+ * can enhance the convolution approximation by considering different
+ * orientations of the kernel.
+ *
+ * @param z Input array to be convolved.
+ * @param kernel 2D kernel to be used for the convolution.
+ * @param rank Approximation rank: number of singular values/vectors used for
+ * the approximation.
+ * @param n_rotations Number of kernel rotations to be considered.
+ * @param seed Random seed number for kernel rotations.
+ * @return Array Resulting array after applying the SVD-based convolution
+ * approximation with rotations.
  *
  * **Example**
  * @include ex_convolve2d_svd.cpp
