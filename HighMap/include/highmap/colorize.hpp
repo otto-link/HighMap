0e3cd7b7ea20e99db220d3466324dd1052682e92
Otto Link <otto.link.bv@gmail.com>
2024-09-20 14:07:56 +0200

    Add slope / elevation heatmap output

--- HighMap/include/highmap/colorize.hpp
+++ HighMap/include/highmap/colorize.hpp
@@ -131,6 +131,46 @@ Tensor colorize_grayscale(const Array &array);
 Tensor colorize_histogram(const Array &array);
 
 /**
+ * @brief Colorizes a slope height heatmap based on the gradient norms of a
+ * given array.
+ *
+ * This function computes a colorized heatmap using a two-dimensional histogram
+ * that considers the gradient norm of the input array. It normalizes both the
+ * input array values and their corresponding gradient norms, calculates a 2D
+ * histogram, and then applies a colormap to visualize the heatmap.
+ *
+ * @param array The input array for which the slope height heatmap is computed.
+ *              This should be a 2D array representing height values.
+ * @param cmap  An integer representing the colormap to be used for
+ * colorization. Colormap options depend on the colorization function used
+ * internally.
+ *
+ * @return Tensor representing the colorized heatmap, which visualizes the
+ * distribution of height values and their corresponding gradient norms.
+ *
+ * @details
+ * - The function normalizes the height values and gradient norms independently
+ * using the minimum and maximum values.
+ * - A 2D histogram is constructed, where each bin corresponds to a pair of
+ * normalized height and gradient values.
+ * - The colormap is then applied to this histogram to produce a colorized
+ * output.
+ *
+ * @note If the input array has a constant value (i.e., min == max), no
+ * normalization is applied, and the function may not produce meaningful
+ * results.
+ *
+ * @warning Ensure that the input array is non-empty and has valid dimensions.
+ *
+ * **Example**
+ * @include ex_colorize_slope_height_heatmap.cpp
+ *
+ * **Result**
+ * @image html ex_colorize_slope_height_heatmap.png
+ */
+Tensor colorize_slope_height_heatmap(const Array &array, int cmap);
+
+/**
  * @brief Combine two arrays into a colored image.
  *
  * This function takes two input arrays and combines them into a single
