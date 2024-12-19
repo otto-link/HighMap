fd49a7294f4c7f7a40cdd30dae45366a9b35a156
Otto Link <otto.link.bv@gmail.com>
2024-12-19 23:44:17 +0100

    Update example

--- examples/ex_gavoronoise/ex_gavoronoise.cpp
+++ examples/ex_gavoronoise/ex_gavoronoise.cpp
@@ -6,8 +6,6 @@ int main(void)
   hmap::gpu::init_opencl();
 
   hmap::Vec2<int> shape = {256, 256};
-  shape = {512, 512};
-  shape = {1024, 1024};
   hmap::Vec2<float> kw = {2.f, 2.f};
   int               seed = 1;
 
