5a6f4a8dd383097a4f3aea1d765a2b33f389ced8
Otto Link <otto.link.bv@gmail.com>
2025-06-15 13:17:28 +0200

    Fix jitter value in Voronoi distance example

--- examples/ex_voronoi_edge_distance/ex_voronoi_edge_distance.cpp
+++ examples/ex_voronoi_edge_distance/ex_voronoi_edge_distance.cpp
@@ -10,21 +10,18 @@ int main(void)
 
   hmap::Array z1 = hmap::gpu::voronoi_edge_distance(shape, kw, seed);
 
-  hmap::Array noise = 0.1f * hmap::noise_fbm(hmap::NoiseType::PERLIN,
+  hmap::Array noise = 0.2f * hmap::noise_fbm(hmap::NoiseType::PERLIN,
                                              shape,
                                              {4.f, 4.f},
                                              seed + 1);
 
-  hmap::Vec2<float> jitter = {0.5f, 1.f};
+  hmap::Vec2<float> jitter = {0.5f, 0.5f};
   hmap::Array       z2 = hmap::gpu::voronoi_edge_distance(shape,
                                                     kw,
                                                     seed,
                                                     jitter,
                                                     nullptr,
                                                     &noise);
-
-  z1.to_png_grayscale("out1.png", CV_16U);
-  z2.to_png_grayscale("out2.png", CV_16U);
 
   hmap::export_banner_png("ex_voronoi_edge_distance.png",
                           {z1, z2},
