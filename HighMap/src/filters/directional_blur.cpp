1a109178748bc8c21b7b624a2e679fd12579f0e3
Otto Link <otto.link.bv@gmail.com>
2025-08-03 00:05:09 +0200

    Fix directional blur

--- HighMap/src/filters/directional_blur.cpp
+++ HighMap/src/filters/directional_blur.cpp
@@ -17,12 +17,12 @@ void directional_blur(Array &array, int ir, const Array &angle, float intensity)
   bool          periodic = false;
   ArrayFunction f = hmap::ArrayFunction(array, Vec2<float>(1.f, 1.f), periodic);
 
-  std::vector<float> t = linspace(intensity, 0.f, ir);
+  std::vector<float> t = linspace(0.f, 1.f, ir);
 
   float tsum = 0.f;
   for (auto &v : t)
   {
-    v = smoothstep3(v);
+    v = intensity * (1.f - smoothstep3(v));
     tsum += v;
   }
 
@@ -30,7 +30,7 @@ void directional_blur(Array &array, int ir, const Array &angle, float intensity)
 
   for (int j = 0; j < array.shape.y; j++)
     for (int i = 0; i < array.shape.x; i++)
-      for (int k = -ir; k < ir; k++)
+      for (int k = -ir + 1; k < ir; k++)
       {
         float alpha = angle(i, j) / 180.f * M_PI;
         float ca = std::cos(alpha);
@@ -43,7 +43,7 @@ void directional_blur(Array &array, int ir, const Array &angle, float intensity)
       }
 
   // try to rescale output
-  array = blured / (1.f + tsum);
+  array = blured / (1.f + 2.f * tsum);
 }
 
 void directional_blur(Array &array, int ir, float angle, float intensity)
